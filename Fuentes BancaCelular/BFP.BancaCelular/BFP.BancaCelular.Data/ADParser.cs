92a3bdca468795c90b164fdcdcaf8dc8fa71ca30
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-02-20 12:25:14 -0300

    Commit inicial

---
+++ Fuentes BancaCelular/BFP.BancaCelular/BFP.BancaCelular.Data/ADParser.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using BFP.BancaCelular.Comun;
+using System.Data.SqlClient;
+
+namespace BFP.BancaCelular.Data
+{
+    public partial class ADParser : ADBase
+    {
+        public IDataReader ObtenerMensajePorNombre(string strNombreMensaje, string strNombreTransaccion)
+        {
+            object[] parametros = new object[] { strNombreMensaje, strNombreTransaccion };
+            IDataReader reader = null;
+
+            try
+            {
+                reader = GetDatabase().ExecuteReader(CConstantes.StoredProcedures.PRO_BC_ObtenerMensajesNombre, parametros);
+
+                return reader;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                parametros = null;
+                reader = null;
+            }
+        }
+
+        public IDataReader ObtenerCamposPorMensaje(int intMensajeID)
+        {
+            object[] parametros = new object[] { intMensajeID };
+            IDataReader reader = null;
+
+            try
+            {
+                reader = GetDatabase().ExecuteReader(CConstantes.StoredProcedures.PRO_BC_ObtenerCamposMensaje, parametros);
+
+                return reader;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                parametros = null;
+                reader = null;
+            }
+        }
+    }
+}
