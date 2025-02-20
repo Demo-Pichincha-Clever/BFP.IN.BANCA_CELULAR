039771fcf307f483ac49ce537944c336fe41f45e
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-02-20 12:48:34 -0300

    Commit inicial

---
+++ Fuentes BancaCelular/BFP.WinServiceBC/BFP.WinService.Entidad/EMensajeOperacion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BFP.WinService.Entidad
+{
+    [Serializable]
+    public class EMensajeOperacion
+    {
+        /// <summary>
+        /// Identificador del mensaje
+        /// </summary>
+        public string IdMensaje { get; set; }
+
+        /// <summary>
+        /// Nombre del mensaje
+        /// </summary>
+        public string NombreMensaje { get; set; }
+
+        /// <summary>   
+        /// Descripcion del mensaje
+        /// </summary>
+        public string DescMensaje { get; set; }
+
+    }
+}
