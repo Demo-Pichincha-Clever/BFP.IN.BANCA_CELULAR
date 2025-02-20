f1032441780bbc45c97d402005742399780bb1fc
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-02-20 12:33:14 -0300

    Commit inicial

---
+++ Fuentes BancaCelular/BFP.WinServiceBC/BFP.WinService.Entidad/EMensajesEnviados.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BFP.WinService.Entidad
+{
+    public class EMensajesEnviados
+    {
+        /// <summary>
+        /// Numero del celular del cliente
+        /// </summary>
+        public string NroCelular { get; set; }
+
+        /// <summary>   
+        /// Tipo Mensajería: A-Afiliacion / D-Desafiliacion
+        /// </summary>
+        public string TipoEvento { get; set; }
+
+        /// <summary>   
+        /// Flag que indica que se envio el SMS
+        /// </summary>
+        public bool EnviaSMS { get; set; }
+
+        /// <summary>   
+        /// Flag que indica que se envio el correo electronico
+        /// </summary>
+        public bool EnviaCorreo { get; set; }
+    }
+}
