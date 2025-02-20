001d56d84767235fea8365ad18f960e146cb63e9
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-02-20 12:29:54 -0300

    Commit inicial

---
+++ Fuentes BancaCelular/BFP.WinServiceBC/BFP.WinService.Data/ADBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Configuration;
+
+namespace BFP.WinService.Data
+{
+    /// <summary>
+    /// Clase base de la capa de acceso a datos.
+    /// </summary>
+    public abstract class ADBase
+    {
+        /// <summary>
+        /// Obtiene el objeto EjecutorBdd.
+        /// </summary>
+        private static Database dbBancaCelular;
+
+        public static Database GetDatabase()
+        {
+            dbBancaCelular = DatabaseFactory.CreateDatabase("dbBancaCelular");
+            return dbBancaCelular;
+        }
+    }
+}
