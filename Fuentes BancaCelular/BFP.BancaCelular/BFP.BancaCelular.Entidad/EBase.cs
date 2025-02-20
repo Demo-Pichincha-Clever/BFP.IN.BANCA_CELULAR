0f166755bf828ce4f65a959a5f65ca4c04f69671
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-02-20 12:42:29 -0300

    Commit inicial

---
+++ Fuentes BancaCelular/BFP.BancaCelular/BFP.BancaCelular.Entidad/EBase.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace BFP.BancaCelular.Entidad
+{
+    /// <summary>
+    /// Clase entidad que describe las propiedades comunes.
+    /// </summary>
+    [Serializable]
+    public class EBase
+    {
+        /// <summary>
+        /// Fecha creación del usuario.
+        /// </summary>
+        public DateTime FechaCreacion { get; set; }
+
+        /// <summary>
+        /// Usuario creador.
+        /// </summary>
+        public string UsuarioCreacion { get; set; }
+
+        /// <summary>
+        /// Fecha creación del usuario.
+        /// </summary>
+        private DateTime mFechaModificacion;
+
+        public DateTime FechaModificacion { get; set; }
+
+        /// <summary>
+        /// Usuario actualizador.
+        /// </summary>
+        public string UsuarioModificacion { get; set; }
+
+        /// <summary>
+        /// Código de actualizacion.
+        /// </summary>
+        private string mTipoActualizacion;
+
+        [XmlIgnore]
+        public string TipoActualizacion
+        {
+            get { return mTipoActualizacion; }
+            set { mTipoActualizacion = value; }
+        }
+
+        /// <summary>
+        /// Código de operación.
+        /// </summary>
+        [XmlIgnore]
+        public string IdOperacion { get; set; }
+
+
+        /// <summary>
+        /// Operacion.
+        /// </summary>
+        [XmlIgnore]
+        public string Operacion { get; set; }
+
+        /// <summary>
+        /// Operacion.
+        /// </summary>
+        [XmlIgnore]
+        public DateTime HoraInicial { get; set; }
+
+    }
+}
