dac034ed0eb44e157dc004aced062ac009a87fa0
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-02-20 12:26:31 -0300

    Commit inicial

---
+++ Fuentes BancaCelular/BFP.BancaCelular/BFP.BancaCelular.Negocio/NGeneral.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using BFP.BancaCelular.Comun;
+using BFP.BancaCelular.Entidad;
+using BFP.BancaCelular.Data;
+
+namespace BFP.BancaCelular.Negocio
+{
+    public static class NGeneral
+    {
+        public static bool VerificarBloqueoTemporalPorDesafiliacion(string strNumeroCelular , int  intOperacionId,  out string strError)
+        {
+            return ADGeneral.VerificarBloqueoTemporalPorDesafiliacion(strNumeroCelular, intOperacionId, out strError);
+        }
+
+        public static void RegistrarIntetosOperacionDia(string strNumeroCelular, int intOperacionId)
+        {
+            ADGeneral.RegistrarIntetosOperacionDia(strNumeroCelular, intOperacionId);
+        }
+    }
+}
