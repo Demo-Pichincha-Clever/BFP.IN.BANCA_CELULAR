72846904651928739783c36b890768700e3e60ff
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-02-21 13:34:17 -0300

    Commit inicial

---
+++ Fuentes BancaCelular/BFP.BancaCelular/BFP.BancaCelular.Entidad/ELogOperaciones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BFP.BancaCelular.Entidad
+{
+
+    [Serializable]
+    public class ELogOperaciones
+    {
+    
+        public string codigoTransaccion { get; set; }
+        public string idOperacion { get; set; }
+        public string descripcionOperacion { get; set; }
+        public string tramaEntrada { get; set; }
+        public string xmlRetorno { get; set; }
+        public DateTime fechaRecepcion { get; set; }
+        public DateTime fechaEnvio { get; set; }
+        public string tipoEvento { get; set; }
+        public string mensajeRetornoError { get; set; }
+        public string codigoTransaccionVerifica { get; set; }
+
+    }
+}
