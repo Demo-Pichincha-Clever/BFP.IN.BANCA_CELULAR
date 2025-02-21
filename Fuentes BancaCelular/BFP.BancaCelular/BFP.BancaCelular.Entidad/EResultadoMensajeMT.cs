3f9a0a78f88fc9285cda1103a8f40777a660ab14
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-02-21 13:20:13 -0300

    Commit inicial

---
+++ Fuentes BancaCelular/BFP.BancaCelular/BFP.BancaCelular.Entidad/EResultadoMensajeMT.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace BFP.BancaCelular.Entidad
+{
+    [Serializable]
+    public class EResultadoMensajeMT
+    {
+        /// <summary>
+        /// Código de retorno
+        /// </summary>
+        public string CodRet { get; set; }
+
+        /// <summary>
+        /// Mensaje de retorno - MT
+        /// </summary>
+        public string MensajeMT { get; set; }
+
+        /// <summary>
+        /// Fecha de la devolución del mensaje MT
+        /// </summary>
+        public string Fecha { get; set; }
+
+
+        /// <summary>
+        /// Hora de la devolución del mensaje MT
+        /// </summary>
+        public string Hora { get; set; }
+
+        /// <summary>
+        /// Fecha de la devolución del mensaje MT
+        /// </summary>
+        [XmlIgnore]
+        public string CodigoError { get; set; }
+
+
+        /// <summary>
+        /// Hora de la devolución del mensaje MT
+        /// </summary>
+        [XmlIgnore]
+        public string MensajeError { get; set; }
+    }
+}
