1bebc281c20491f573595bc4423c93de5aead7cd
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-02-21 13:34:14 -0300

    Commit inicial

---
+++ Fuentes BancaCelular/BFP.BancaCelular/BFP.BancaCelular.Entidad/ETrama.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace BFP.BancaCelular.Entidad
+{
+
+    /// <summary>
+    /// Entidad de trama
+    /// </summary>
+    [Serializable]
+    public class ETrama : EBase
+    {
+        /// <summary>
+        /// Entrada
+        /// </summary>
+        public string Entrada { get; set; }
+
+        /// <summary>
+        /// Salida
+        /// </summary>
+        public string Salida { get; set; }
+    }
+}
+
