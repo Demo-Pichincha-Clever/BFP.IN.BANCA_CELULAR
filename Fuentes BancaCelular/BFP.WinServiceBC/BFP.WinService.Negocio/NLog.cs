edb9cb2e2e15f355a8165a05fe86b2f9c72e4ecb
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-02-21 13:25:56 -0300

    Commit inicial

---
+++ Fuentes BancaCelular/BFP.WinServiceBC/BFP.WinService.Negocio/NLog.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using BFP.WinService.Comun;
+using BFP.WinService.Entidad;
+using BFP.WinService.Data;
+
+namespace BFP.WinService.Negocio
+{
+    public static class NLog
+    {
+        /// <summary>
+        /// Insertar log de eventos
+        /// </summary>
+        /// <param name="log"></param>
+        public static void Insertar(string idUsuario, ECatalogo<string, string> objOpe,
+                                    string TipoEvento, string Origen,
+                                    string Entrada, string Salida, DateTime HoraInicial, int codigoApp)
+        {
+            ELog log = new ELog();
+
+            if (idUsuario != null)
+            {
+                log.Usuario = idUsuario;
+            }
+            else
+            {
+                log.Usuario = string.Empty;
+            }
+
+            if (objOpe != null)
+            {
+                log.IdOperacion = objOpe.Valor;
+                log.DescripcionOperacion = objOpe.Nombre;
+            }
+            else
+            {
+                log.IdOperacion = string.Empty;
+                log.DescripcionOperacion = string.Empty;
+            }
+
+            log.HoraInicial = HoraInicial;
+            log.TipoEvento = TipoEvento;
+            log.Origen = Origen;
+            log.Entrada = Entrada;
+            log.Salida = Salida;
+            log.HoraFinal = DateTime.Now;
+            log.CodigoApp = codigoApp;
+
+            ADLog.Insertar(log);
+        }
+    }
+}
