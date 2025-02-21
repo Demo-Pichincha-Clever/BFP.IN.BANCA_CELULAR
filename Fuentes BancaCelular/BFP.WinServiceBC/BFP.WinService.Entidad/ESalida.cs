fe1ad66b6f0f9295b27e89df44a6b716c30f3486
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-02-21 13:39:39 -0300

    Commit inicial

---
+++ Fuentes BancaCelular/BFP.WinServiceBC/BFP.WinService.Entidad/ESalida.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace BFP.WinService.Entidad
+{
+    /// <summary>
+    /// Clase entidad que describe las propiedades de salida para la mensajería de seguridad.
+    /// </summary>
+    /// <typeparam name="T">Tipo de dato del código.</typeparam>
+    [Serializable]
+    public class ESalida<T>
+    {
+        /// <summary>
+        /// Código.
+        /// </summary>
+        [XmlElement("codigo")]
+        public T Codigo { get; set; }
+
+        /// <summary>
+        /// Descripción del mensaje.
+        /// </summary>
+        [XmlElement("descripcion")]
+        public string Mensaje { get; set; }
+    }
+}
