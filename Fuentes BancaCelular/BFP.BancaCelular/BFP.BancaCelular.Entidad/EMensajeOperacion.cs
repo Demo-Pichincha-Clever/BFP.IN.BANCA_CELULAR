cb62919ee26eaf3e97fd65b6aa2460f864bfa27c
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-02-21 13:20:02 -0300

    Commit inicial

---
+++ Fuentes BancaCelular/BFP.BancaCelular/BFP.BancaCelular.Entidad/EMensajeOperacion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BFP.BancaCelular.Entidad
+{
+    [Serializable]
+    public class EMensajeOperacion
+    {
+        /// <summary>
+        /// Identificador del mensaje
+        /// </summary>
+        public string IdMensaje { get; set; }
+
+        /// <summary>
+        /// Nombre del mensaje
+        /// </summary>
+        public string NombreMensaje { get; set; }
+
+        /// <summary>   
+        /// Descripcion del mensaje
+        /// </summary>
+        public string DescMensaje { get; set; }
+    }
+}
