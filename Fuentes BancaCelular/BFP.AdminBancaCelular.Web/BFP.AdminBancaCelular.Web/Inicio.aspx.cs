25c2f58db4b18505ee626a7d26d3336314faf35a
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-02-21 13:41:27 -0300

    Commit inicial

---
+++ Fuentes BancaCelular/BFP.AdminBancaCelular.Web/BFP.AdminBancaCelular.Web/Inicio.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Xml.Linq;
+using System.Collections.Generic;
+
+using BFP.AdminBancaCelular.Entidad;
+using BFP.AdminBancaCelular.Negocio;
+ 
+namespace BFP.AdminBancaCelular.Web
+{
+    public partial class Inicio : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+ 
+        }
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            if (Request.ServerVariables["http_user_agent"].IndexOf("Safari", StringComparison.CurrentCultureIgnoreCase) != -1)
+                Page.ClientTarget = "uplevel";
+        }
+
+    }
+}
