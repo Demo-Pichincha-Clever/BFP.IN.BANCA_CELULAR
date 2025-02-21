e9e8eabb9a235f6b9638b8d1859631247051e68f
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-02-21 13:39:21 -0300

    Commit inicial

---
+++ Fuentes BancaCelular/BFP.WinServiceBC/BFP.WinService.Entidad/EMensajeria.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BFP.WinService.Entidad
+{
+    public class EMensajeria
+    {
+        /// <summary>
+        /// Numero del celular del cliente
+        /// </summary>
+        public string NroCelular { get; set; }
+
+        /// <summary>   
+        /// Email del cliente
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>   
+        /// Canal de emision: HB-HomeBanking, VT-Ventanilla, PL-Plataforma, BT-Banca Telefonica
+        /// </summary>
+        public string Canal { get; set; }
+
+        /// <summary>
+        /// Indica si el mensaje fue enviado
+        /// </summary>
+        public bool EnvioSMS { get; set; }
+
+        /// <summary>
+        /// Indica si el mensaje fue enviado
+        /// </summary>
+        public bool EnvioEmail { get; set; }
+
+        /// <summary>
+        /// Indica si el mensaje fue enviado
+        /// </summary>
+        public string CodigoOperadora { get; set; }
+
+    }
+}
